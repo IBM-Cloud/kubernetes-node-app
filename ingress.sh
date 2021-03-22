c8af824e057e1d013025e1ce88739bd471ebe309
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2021-03-22 22:56:34 +0530

    Added yaml files and script

---
+++ ingress.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+function ibmdomain() {
+    cat yaml-templates/ingress-ibmdomain-template.yaml | \
+        MYPROJECT=$MYPROJECT \
+        INGRESS_SUBDOMAIN=$INGRESS_SUBDOMAIN \
+        INGRESS_SECRET_NAME=$INGRESS_SECRET_NAME \
+        KUBERNETES_NAMESPACE=$KUBERNETES_NAMESPACE \
+        envsubst > ingress-ibmdomain.yaml
+}
+
+function customdomain_http() {
+    cat yaml-templates/ingress-customdomain-http-template.yaml | \
+        MYPROJECT=$MYPROJECT \
+        CUSTOM_DOMAIN=$CUSTOM_DOMAIN \
+        KUBERNETES_NAMESPACE=$KUBERNETES_NAMESPACE \
+        envsubst > ingress-customdomain-http.yaml
+}
+
+function customdomain_https() {
+    cat yaml-templates/ingress-customdomain-https-template.yaml | \
+    MYPROJECT=$MYPROJECT \
+    CUSTOM_DOMAIN=$CUSTOM_DOMAIN \
+    INGRESS_SECRET_NAME=$INGRESS_SECRET_NAME \
+    KUBERNETES_NAMESPACE=$KUBERNETES_NAMESPACE \
+    envsubst > ingress-customdomain-https.yaml
+}
+
+function log_error() {
+    MSG="Unknown function or something went wrong...."
+    printf "%s - [ERROR] %s\n" "$(date)" "$MSG" >&2
+}
+
+case "$1" in
+    "") ;;
+    ibmdomain) "$@"; exit;;
+    customdomain_http) "$@"; exit;;
+    customdomain_https) "$@"; exit;;
+    *) log_error "Unknown function: $1()"; exit 2;;
+esac
+
+
