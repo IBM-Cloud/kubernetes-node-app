19d857c7d9f9875dbdf940bc71e7ba9347b40e62
Henrik Loeser <hloeser@de.ibm.com>
2024-06-18 14:02:05 +0200

    use SM integration and CLI

--- ingress.sh
+++ ingress.sh
@@ -29,6 +29,15 @@ function customdomain_https() {
     envsubst > ingress-customdomain-https.yaml
 }
 
+function customdomain_https_sm() {
+    cat yaml-templates/ingress-customdomain-https-sm-template.yaml | \
+    MYAPP=$MYAPP \
+    CUSTOM_SUBDOMAIN=$CUSTOM_SUBDOMAIN \
+    CUSTOM_SECRET_NAME=$CUSTOM_SECRET_NAME \
+    KUBERNETES_NAMESPACE=$KUBERNETES_NAMESPACE \
+    envsubst > ingress-customdomain-https.yaml
+}
+
 function log_error() {
     MSG="Unknown function or something went wrong...."
     printf "%s - [ERROR] %s\n" "$(date)" "$MSG" >&2
@@ -39,6 +48,7 @@ case "$1" in
     ibmsubdomain_https) "$@"; exit;;
     customdomain_http) "$@"; exit;;
     customdomain_https) "$@"; exit;;
+    customdomain_https_sm) "$@"; exit;;
     *) log_error "Unknown function: $1()"; exit 2;;
 esac
 
